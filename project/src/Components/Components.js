6d7cdb6357415358cb4a41bcb13dbd0b4601ba73
nik1tsyganov <71796872+nik1tsyganov@users.noreply.github.com>
2022-06-29 16:29:45 +0200

    fixed route (maybe)

--- project/src/Components/Components.js
+++ project/src/Components/Components.js
@@ -2,17 +2,19 @@ import React from "react";
 import Historical from "./Historical/Historical";
 import Home from "./Home/Home";
 import Footer from "./Footer/Footer";
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 
 const Components = () => {
   return (
     <Router>
       <div>
+      <Routes>
         {/* <Home /> */}
-        <Route path="/" exact component={Home} />
+        <Route path="/" exact element={<Home/>} />
         {/* <Historical /> */}
-        <Route path="/Historical" component={Historical} />
+        <Route path="/Historical" element={<Historical/>} />
+        </Routes>
       </div>
       <Footer />
     </Router>
