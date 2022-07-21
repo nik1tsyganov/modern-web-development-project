2b367dc5a3609d9b3794ccf7b6ae150f124bc3ae
nik1tsyganov <71796872+nik1tsyganov@users.noreply.github.com>
2022-07-21 12:44:02 +0200

    undo deployment changes

--- html/src/Common/Services/SearchService.js
+++
@@ -1,9 +0,0 @@
-export const containsWord = (body, word) => {
-
-    var arr = body.split(" ");
-    if (arr.find(item => word.toLowerCase() === item.toLowerCase())) {
-        return true;
-    }
-    
-    return false;
-};
--- html/src/environments.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-  APPLICATION_ID: "ipwSau4LG87NbG29vhyePDH7Jm7tdNmgHeMZKxGE",
-  JAVASCRIPT_KEY: "4ao1IhYLXQKGkwayuMGTp9enCHs6AHHAueMsDR5z",
-  SERVER_URL: "https://parseapi.back4app.com/"
-};
